5d5a6223382993be008ca492ca8b9734deaae9db
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-03-20 12:42:02 +0100

    basic class added

---
+++ main.py
@@ -0,0 +1,17 @@
+
+class Channel:
+    def __init__(self,binary_string) -> None:
+        pass
+
+    def disrupt():
+        print()
+
+class Message:
+    def __init__(self) -> None:
+        pass
+
+def main():
+    print()
+
+
+main()
