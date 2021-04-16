a49fe3518aad9fac41ba8b64c3ce95bde4da4ef2
KamilB00 <bonkowski.kamil@icloud.com>
2021-04-16 20:09:20 +0200

    imports added

--- Message.py
+++ Message.py
@@ -1,5 +1,7 @@
 import base64
 import binascii
+from functools import reduce
+from operator import xor
 
 test_image_file_name = "test_image.jpeg"
 test_binary_file_name = "binary_image.bin"
@@ -9,10 +11,11 @@ saved_test_image = "save_image.jpeg"
 def image_file_to_bin_file(image_file_name, binary_file_name):
     """Load image file (.jpg), convert to bytes string then save to .bin file
     """
-    with open(image_file_name,"rb") as loaded_image:
+    with open(image_file_name, "rb") as loaded_image:
         image_as_string = base64.b64encode(loaded_image.read())
-    with open(binary_file_name,"wb") as saved_binary:
+    with open(binary_file_name, "wb") as saved_binary:
         saved_binary.write(image_as_string)
+
 
 def bin_file_to_image_file(image_file_name, binary_file_name):
     """Load .bin file then save as image file (.jpg)
@@ -23,12 +26,14 @@ def bin_file_to_image_file(image_file_name, binary_file_name):
     destination_image_file.write(base64.b64decode((file_bytes)))
     destination_image_file.close()
 
-def bytes_string_to_image_file(bytes_string,image_file_name):
+
+def bytes_string_to_image_file(bytes_string, image_file_name):
     """Save byte string to image file (.jpg)
     """
     destination_image_file = open(image_file_name, "wb")
     destination_image_file.write(base64.b64decode((bytes_string)))
     destination_image_file.close()
+
 
 def bin_file_to_bytes_string(binary_file_name):
     """Load .bin file then return as bytes string
@@ -37,11 +42,13 @@ def bin_file_to_bytes_string(binary_file_name):
     file_bytes = binary_source.read().decode('utf-8')
     return file_bytes
 
+
 def bytes_string_to_binary_string(text, encoding='utf-8', errors='surrogatepass'):
     """Convert bytes string (e.g. agO3;) to binary string (e.g. 100101001000) then return binary string
     """
     bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
+
 
 def binary_string_to_bytes_string(bits, encoding='utf-8', errors='surrogatepass'):
     """Convert binary string (e.g. 100100101) to bytes string (e.g. fE4%) then return bytes string
@@ -49,19 +56,22 @@ def binary_string_to_bytes_string(bits, encoding='utf-8', errors='surrogatepass'
     n = int(bits, 2)
     return int2bytes(n).decode(encoding, errors)
 
+
 def int2bytes(i):
     hex_string = '%x' % i
     n = len(hex_string)
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
+
 def image_to_binary_string(image_file_name):
     """Load image (e.g. jpg) and return as binary string
     """
-    with open(image_file_name,"rb") as loaded_image:
+    with open(image_file_name, "rb") as loaded_image:
         image_as_string = base64.b64encode(loaded_image.read()).decode('utf-8')
     return bytes_string_to_binary_string(image_as_string)
 
-def binary_string_to_image(binary_string,image_file_name):
+
+def binary_string_to_image(binary_string, image_file_name):
     """Save binary string as image (e.g. jpg)
     """
     bytes_to_save = binary_string_to_bytes_string(binary_string)
