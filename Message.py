d95cec2ded51d8883eecafbb2007277b418b1c57
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-05-01 23:23:17 +0200

    image io update

--- Message.py
+++ Message.py
@@ -1,85 +1,30 @@
-import base64
-import binascii
+from PIL import Image
+import numpy as np
 from functools import reduce
 from operator import xor
 
 test_image_file_name = "test_image.jpeg"
-test_binary_file_name = "binary_image.bin"
 saved_test_image = "save_image.jpeg"
 
+class Image_message:
+    def __init__(self,file_name):
+        """Load data from file and get size and pixel data (as 'image_bits')
+        """
+        try:
+            image  = Image.open(file_name)
+        except IOError:
+            print("Image loading error")
+        self.size = image.size
+        pixels = np.asarray(image, dtype=np.uint8)
+        self.image_bits = np.unpackbits(pixels)
 
-def image_file_to_bin_file(image_file_name, binary_file_name):
-    """Load image file (.jpg), convert to bytes string then save to .bin file
-    """
-    with open(image_file_name, "rb") as loaded_image:
-        image_as_string = base64.b64encode(loaded_image.read())
-    with open(binary_file_name, "wb") as saved_binary:
-        saved_binary.write(image_as_string)
-
-
-def bin_file_to_image_file(image_file_name, binary_file_name):
-    """Load .bin file then save as image file (.jpg)
-    """
-    binary_source = open(binary_file_name, "rb")
-    file_bytes = binary_source.read()
-    destination_image_file = open(image_file_name, "wb")
-    destination_image_file.write(base64.b64decode((file_bytes)))
-    destination_image_file.close()
-
-
-def bytes_string_to_image_file(bytes_string, image_file_name):
-    """Save byte string to image file (.jpg)
-    """
-    destination_image_file = open(image_file_name, "wb")
-    destination_image_file.write(base64.b64decode((bytes_string)))
-    destination_image_file.close()
-
-
-def bin_file_to_bytes_string(binary_file_name):
-    """Load .bin file then return as bytes string
-    """
-    binary_source = open(binary_file_name, "rb")
-    file_bytes = binary_source.read().decode('utf-8')
-    return file_bytes
-
-
-def bytes_string_to_binary_string(text, encoding='utf-8', errors='surrogatepass'):
-    """Convert bytes string (e.g. agO3;) to binary string (e.g. 100101001000) then return binary string
-    """
-    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
-
-
-def binary_string_to_bytes_string(bits, encoding='utf-8', errors='surrogatepass'):
-    """Convert binary string (e.g. 100100101) to bytes string (e.g. fE45) then return bytes string
-    """
-    n = int(bits, 2)
-    return int2bytes(n).decode(encoding, errors)
-
-
-def int2bytes(i):
-    hex_string = '%x' % i
-    n = len(hex_string)
-    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
-
-
-def image_to_binary_string(image_file_name):
-    """Load image (e.g. jpg) and return as binary string
-    """
-    #sczytać tablicę pikseli
-    with open(image_file_name, "rb") as loaded_image:
-        image_as_string = base64.b64encode(loaded_image.read()).decode('utf-8')
-    return bytes_string_to_binary_string(image_as_string)
-
-
-def binary_string_to_image(binary_string, image_file_name):
-    """Save binary string as image (e.g. jpg)
-    """
-    bytes_to_save = binary_string_to_bytes_string(binary_string)
-    destination_image_file = open(image_file_name, "wb")
-    destination_image_file.write(base64.b64decode((bytes_to_save)))
-    destination_image_file.close()
-
+    def save(self,file_name):
+        """Save image to file with current pixel data (as 'image_bits')
+        """
+        packed_bits = np.packbits(self.image_bits)
+        packed_bits_reshaped = np.reshape(packed_bits,(self.size[1],self.size[0],3))
+        image = Image.fromarray(packed_bits_reshaped)
+        image.save(file_name)
 
 def bits_trippling(binary_string):
     """Function which tripple bits in the binary sting eg(101 -> 111000111)
