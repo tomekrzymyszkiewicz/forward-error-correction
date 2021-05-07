59765d6524f7d70fca062bbdb19a6b8c23157cec
wikorp <61712540+wikorp@users.noreply.github.com>
2021-05-07 21:11:52 +0200

    haming code code/decode

--- Message.py
+++ Message.py
@@ -51,6 +51,99 @@ class Image_message:
                     decoded_binary_image_bits[i] = third[i]
             return decoded_binary_image_bits
 
+class CorectionCodes:
+    def __init__(self):
+        """Load message bits
+        """
+
+    def __init__(self,message_bits):
+        """Load message bits
+        """
+        self.message_bits = message_bits
+
+
+    def hamming_code(self, message_bits):
+        """Hamming code(7,4) 
+        """
+        haming_coded_bits = []
+        i = 0
+        j = 0
+        while i < len(message_bits):
+            if i + 3 >= len(message_bits):
+                break
+            for x in range(0, 7):
+                haming_coded_bits.append(0)
+        
+            haming_coded_bits[j] = message_bits[i]
+            haming_coded_bits[j + 1] = message_bits[i + 1]
+            haming_coded_bits[j + 2] = message_bits[i + 2]
+            haming_coded_bits[j + 4] = message_bits[i + 3]
+
+            redundancy = 0
+            iterator = 0
+            for x in range(7, 0, -1):
+                if haming_coded_bits[j + iterator] == 1:
+                    redundancy = redundancy ^ x
+                iterator += 1    
+        
+            haming_coded_bits[j + 3] = int(redundancy & 4 > 0)
+            haming_coded_bits[j + 5] = int(redundancy & 2 > 0)
+            haming_coded_bits[j + 6] = int(redundancy & 1 > 0)
+            
+            i += 4
+            j += 7
+        while i < len(message_bits):
+            haming_coded_bits.append(0)
+            haming_coded_bits[j] = message_bits[i]
+            haming_coded_bits[j] = message_bits[i]
+            i += 1
+
+
+    def decode_hamming_code(self, haming_coded_bits):
+        """decode Hamming code(7,4) 
+        """
+        decoded_haming_coded_bits = []
+        i = 0
+        j = 0
+        while i < len(haming_coded_bits):
+            if i + 6 >= len(haming_coded_bits):
+                break
+            for x in range(0, 4):
+                decoded_haming_coded_bits.append(0)
+    
+            decoded_haming_coded_bits[j] = haming_coded_bits[i]
+            decoded_haming_coded_bits[j + 1] = haming_coded_bits[i + 1]
+            decoded_haming_coded_bits[j + 2] = haming_coded_bits[i + 2]
+            decoded_haming_coded_bits[j + 3] = haming_coded_bits[i + 4]
+    
+            detection = 0
+            iterator = 0
+            for x in range(7, 0, -1):
+                if haming_coded_bits[i + iterator] == 1:
+                    detection = detection ^ x
+                iterator += 1    
+
+            if detection == 7:
+                decoded_haming_coded_bits[i] =  int(not decoded_haming_coded_bits[i])
+            if detection == 6:
+                decoded_haming_coded_bits[i + 1] =  int(not decoded_haming_coded_bits[i])
+            if detection == 5:
+                decoded_haming_coded_bits[i + 2] =  int(not decoded_haming_coded_bits[i])
+            if detection == 3:
+                decoded_haming_coded_bits[i + 3] =  int(not decoded_haming_coded_bits[i])
+              
+            i += 7
+            j += 4
+
+        while i < len(haming_coded_bits):          #NIE WIELOKROTNOSCI 4/7
+            decoded_haming_coded_bits.append(0)
+            decoded_haming_coded_bits[j] = haming_coded_bits[i]
+            decoded_haming_coded_bits[j] = haming_coded_bits[i]
+            i += 1
+
+
+
+            
 
 def hamming_code(self, binary_string):
     """Hamming code - error corection code finding single error 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Channel import Channel
 from Message import Image_message
+from Message import CorectionCodes
 
 test_image_file_name = "test_image.jpeg"
 saved_test_image = "save_image.jpeg"
@@ -12,5 +13,14 @@ def main():
     decoded__message = Image_message.decode_trippled_bits(image,trippled_message_with_errors)
     image.image_bits = decoded__message
     image.save(saved_test_image)
+    
+    #moja propozycja organizacji - rozbicie kodow korekcyjnych na dodatkowa klase#
+    
+    #message = Image_message(test_image_file_name)
+    #haming_coded_message = CorectionCodes.hamming_code(message.image_bits)
+    #haming_coded_message_with_errors = Channel.random_error_number(haming_coded_message, 4000000)
+    #decoded_haming_coded_message = CorectionCodes.decode_hamming_code(haming_coded_message_with_errors)
+    #message.image_bits = decoded_haming_coded_message
+    #message.save(saved_test_image)
 
 main()
