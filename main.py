e9ea66083ebdcd2f19b637b2819217d83947e1ae
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-05-24 19:23:19 +0200

    old stuff deleted

--- main.py
+++ main.py
@@ -57,37 +57,9 @@ def main():
     image.image_bits = decoded__message
     image.save(saved_test_image)
 
-#####################################################################################################################################
-    #image = Image_message(test_image_file_name)
-
-    # encoded_message = image.bits_trippling_1() # bits trippling
-    # encoded_message = image.bits_trippling_2() # bits trippling
-    # encoded_message = image.hamming_encode(10)
-    #encoded_message = image.BCH_encode(10,2)
-    # encoded_message = image.ParityCheck_encode(10)
-
-    #number_of_arrays = len(encoded_message)
-    #concated = np.concatenate(encoded_message)
-    
-    #channel = Channel(encoded_message) # bits trippling
-    #channel = Channel(concated)
-    #encoded_message_with_errors = Channel.random_error_number(concated, 100000) #tu concatenate
-    #encoded_meassage_with_error_to_decode = image.array_to_decode(number_of_arrays,encoded_message_with_errors)
-
-    # decoded_message = image.decode_trippled_bits(encoded_message_with_errors,'F') # bits trippling decode AAABBBCCC
-    # decoded_message = image.decode_trippled_bits(encoded_message_with_errors,'C') # bits trippling decode ABCABCABC
-    # decoded_message = image.hamming_decode(encoded_meassage_with_error_to_decode,10)
-    #decoded_message = image.BCH_decode(encoded_meassage_with_error_to_decode,10,2) #to mozna dac wewnatrz kazdej funkcji
-    # decoded_message = image.ParityCheck_decode(encoded_meassage_with_error_to_decode,10)
-    #image.image_bits = decoded_message
-
-
-    image.save(saved_test_image)
-
     results.add_result(image.image_bits,concated,decoded_message,'BCD',100)
     results.print_results()
     results.save_to_file(results_file_name)
-    #image.save(saved_test_image)
   
 
 main()
